7ff626a05520cfbd91ab7c6f056eb7c85fdff4ed
sideeqhammed <sideeqhammed05.com>
2025-08-19 21:51:29 +0100

    added MySQLServer.py

---
+++ task_2.sql
@@ -0,0 +1,37 @@
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Authors (
+  author_id PRIMARY KEY,
+  author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+CREATE TABLE Orders (
+  order_id INT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+  orderdetailid PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
