7f18b6807e3416ea1b767f322fec49994c1022c2
sideeqhammed <sideeqhammed05.com>
2025-08-19 20:18:18 +0100

    added docker compose.yml & alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,13 +23,16 @@ CREATE TABLE Customers (
 );
 
 CREATE TABLE Orders (
-  order_id (Primary Key),
+  order_id INT PRIMARY KEY,
+  customer_id INT,
   order_date DATE,
   FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
   orderdetailid PRIMARY KEY,
+  order_id INT,
+  book_id INT,
   quantity DOUBLE,
   FOREIGN KEY (order_id) REFERENCES Orders(order_id),
   FOREIGN KEY (book_id) REFERENCES Books(book_id)
