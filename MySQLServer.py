f956f0490401cf390f49d35d7276cd9ed921de92
sideeqhammed <sideeqhammed05.com>
2025-08-19 21:37:05 +0100

    added MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,16 @@
+from mysql.connector import connect, Error
+from getpass import getpass
+
+
+try:
+  with connect(
+    host = "localhost",
+    port= 3308,
+    user = input("Enter your username: "),
+    password = getpass("Enter your password: ")
+  ) as connection:
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+
+except Error as e:
+  print(e)
